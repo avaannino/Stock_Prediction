4b2f9de8ab4254bc11d52ab97ead93d11e1b4f5f
avaannino <ava.annino@tcu.edu>
2026-04-21 15:27:55 -0500

    Refactor Streamlit app for clarity and structure

--- Portfolio/StreamlitApp_HW6.py
+++ Portfolio/StreamlitApp_HW6.py
@@ -136,7 +136,7 @@ def display_explanation(input_df, session, aws_bucket):
     #shap.plots.waterfall(shap_values[0, :, 0])
     st.pyplot(fig)
     # top feature 
-    top_feature = pd.Series(shap_values[0].values, index=shap_values[0].feature_names).abs().idxmax() (FOR OPTION 1)
+    top_feature = pd.Series(shap_values[0].values, index=shap_values[0].feature_names).abs().idxmax()
     #top_feature = pd.Series(shap_values[0, :, 0].values, index=shap_values[0, :, 0].feature_names).abs().idxmax()
     st.info(f"**Business Insight:** The most influential factor in this decision was **{top_feature}**.")
 
